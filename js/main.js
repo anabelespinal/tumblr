e700f45a6441506ed15e8d15546fab187188e691
anbelespinal <anabelespinal>
2016-10-06 18:00:07 -0500

    tumblr

---
+++ js/main.js
@@ -0,0 +1,182 @@
+window.addEventListener("load", function(){
+	publicar.addEventListener("click", btnPublicar);
+	publicarTexto.addEventListener("click", publTexto);
+	publicarCita.addEventListener("click", publCita);
+	publicarMeme.addEventListener("click", publMeme);
+	publicarPastilla.addEventListener("click", publPastilla);
+ });
+
+var publicar = document.getElementsByName("publicar")[0];
+var publicarTexto = document.getElementsByName("texto")[0];
+var publicarCita = document.getElementsByName("cita")[0];
+var publicarMeme = document.getElementsByName("meme")[0];
+var publicarPastilla = document.getElementsByName("pastilla")[0];
+var section = document.getElementsByTagName("section")[0];
+
+function btnPublicar(){
+	var none = document.getElementsByTagName("div")[3];
+	none.style.display = "none";
+}
+
+// funcion para el evento de publicar texto
+function publTexto(){
+	publicar.value = "TEXTO";
+	var contenedorPorPublicar = contenedorPublicar();
+	var agregarContenedor = agregandoContenedor(contenedorPorPublicar);
+	var creandoTitulo = creandoInput(contenedorPorPublicar);
+	creandoTitulo.placeholder = "INSERTA EL TITULO DE TU TEXTO"
+	var creandotextArea =  creandoTextarea(contenedorPorPublicar);
+	var creandoBoton = agregandoBoton(contenedorPorPublicar);
+	creandoBoton.addEventListener("click", function(){
+		var con = creandoContenedorPublicados(creandotextArea.value);
+		insertarTitulo(creandoTitulo.value, con);
+		hora(con);
+	});
+}
+
+
+// funcion para el evento publicar cita
+
+function publCita(){
+	publicar.value = "CITA";
+	var nuevoContenedor = contenedorPublicar();
+	var agregarContenedor = agregandoContenedor(nuevoContenedor);
+	var creandotextArea =  creandoTextarea(nuevoContenedor);
+	var creandoBoton = agregandoBoton(nuevoContenedor);
+	var input = creandoInput(nuevoContenedor);
+	creandoBoton.addEventListener("click", function(){
+		var pub = creandoContenedorPublicados(creandotextArea.value);
+		var sp = crearSpan(input.value, pub);
+		hora(pub);
+	});
+}
+
+
+// funcion para el eveneto publicar meme
+
+function publMeme(){
+	publicar.value = "MEME";
+	var nuevoContenedor = contenedorPublicar();
+	var agregarContenedor = agregandoContenedor(nuevoContenedor);
+	var input = creandoInput(nuevoContenedor);
+	input.placeholder = "ESCRIBE EL LINK DEL MEME"
+	var creandoBoton = agregandoBoton(nuevoContenedor);
+	creandoBoton.addEventListener("click", function(){
+		var imagen = creandoImagen(input.value, ana());
+		hora(ana());
+	});	
+}
+// funcion para el evento de publicar pastilla
+
+function publPastilla(){
+	publicar.value = "PASTILLAS";
+	var nuevoContenedor = contenedorPublicar();
+	// var contenedorPorPublicar = contenedorPublicar();
+	var agregarContenedor = agregandoContenedor(nuevoContenedor);
+	var creandotextArea =  creandoTextarea(nuevoContenedor);
+	var fondo = cambiarFondo(nuevoContenedor);
+	var creandoBoton = agregandoBoton(nuevoContenedor);
+	creandoBoton.addEventListener("click", function(){
+		var cont = creandoContenedorPublicados(creandotextArea.value);
+		var color = colorFondo(cont, fondo);
+		hora(cont);
+	});
+}
+
+
+
+
+// empezando a crear las partes
+function contenedorPublicar(){
+	var contenedorPorPublicar = document.createElement("div");
+	contenedorPorPublicar.setAttribute("class", "contenedorPorPublicar");
+	contenedorPorPublicar.classList.add("col", "s12", "m6");
+	return contenedorPorPublicar;
+}
+function agregandoContenedor(contenedor){
+	section.insertBefore(contenedor, section.children[1]);
+	return contenedor;
+}
+function creandoTextarea(contenedor){
+	var textArea = document.createElement("textarea");
+	contenedor.appendChild(textArea);
+	return textArea;
+}
+function agregandoBoton(contenedor){
+	var boton = document.createElement("button");
+	boton.innerText = "publicar";
+	boton.setAttribute("type", "button");
+	contenedor.appendChild(boton);
+	return boton;
+}
+function insertarTitulo(valor, content){
+		var titulo = document.createElement("div");
+		titulo.setAttribute("class", "titulo")
+		titulo.innerText = valor;
+		content.insertBefore(titulo, content.childNodes[0]);
+		return titulo;
+}
+function creandoContenedorPublicados(valor){
+	var contenedorPublicado = document.createElement("div");
+	contenedorPublicado.setAttribute("class", "creandoContenedorPublicados");
+	section.insertBefore(contenedorPublicado, section.children[2]);
+	creandoDivPublicacion(contenedorPublicado, valor);
+	return contenedorPublicado;
+}
+function ana(){
+	var contenedorPublicado = document.createElement("div");
+	contenedorPublicado.setAttribute("class", "creandoContenedorPublicados");
+	section.insertBefore(contenedorPublicado, section.children[2]);
+	return contenedorPublicado;
+}
+function creandoDivPublicacion(contenedorpublicado, valor){
+	var creandoDiv = document.createElement("div");
+	creandoDiv.innerText = valor;
+	contenedorpublicado.insertBefore(creandoDiv, contenedorpublicado.children[0]);
+	return creandoDiv;
+}
+function creandoInput(content){
+	var nuevoInput = document.createElement("input");
+	nuevoInput.placeholder = "ESCRIBE EL NOMBRE DEL AUTOR";
+	content.insertBefore(nuevoInput, content.children[0]);
+	return nuevoInput;
+}
+function crearSpan(valor, content){
+	var nuevoSpan = document.createElement("span");
+	nuevoSpan.setAttribute("class", "crearSpan")
+	nuevoSpan.innerText = "Autor: " + valor;
+	content.insertBefore(nuevoSpan, content.children[1]);
+	return nuevoSpan;
+}
+function creandoImagen(valor, content){
+	var img = document.createElement("img");
+	img.setAttribute("src", valor);
+	img.setAttribute("class","z-depth-3")
+	content.insertBefore(img, content.children[0]);
+	return img;
+}
+function cambiarFondo(content){
+	var bobtonColor = document.createElement("input");
+	bobtonColor.setAttribute("type", "color");
+	// bobtonColor.innerText = "cambiar color de fondo";
+	content.appendChild(bobtonColor);
+	return bobtonColor;
+}
+function colorFondo(content, input){
+	var colorInput = content.style.backgroundColor = input.value;
+	return colorInput;
+}
+function hora(contenido){
+	var contHora = document.createElement("div");
+	contHora.setAttribute("class", "contHora")
+	var horaPublicar = new Date(); 
+	var spanUno = document.createElement("span");
+	spanUno.innerText = horaPublicar.getDate() + "/" + (horaPublicar.getMonth() + 1) + "/" + horaPublicar.getFullYear() + "    ";
+	contHora.appendChild(spanUno);
+	var spanDos = document.createElement("span");
+	spanDos.innerText = horaPublicar.getHours() + ";" + horaPublicar.getMinutes()+ ";" + horaPublicar.getSeconds(); 
+	contHora.appendChild(spanDos);
+	contenido.appendChild(contHora);
+	return contHora;
+}
+// horaPublicar.getDate() + "/" + (horaPublicar.getMonth() + 1) + "/" + horaPublicar.getFullYear()
