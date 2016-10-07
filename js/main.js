da609f75c851bd0ef41b70561861f1e81df88b0d
anbelespinal <anabelespinal>
2016-10-07 16:10:35 -0500

    nombrando las funciones

--- js/main.js
+++ js/main.js
@@ -83,6 +83,8 @@ function publPastilla(){
 }
 // empezando a crear las partes
 
+
+// creando el contenedor
 function contenedorPublicar(){
 	var contenedorPorPublicar = document.createElement("div");
 	contenedorPorPublicar.setAttribute("class", "contenedorPorPublicar");
@@ -93,11 +95,17 @@ function agregandoContenedor(contenedor){
 	array[5].insertBefore(contenedor, array[5].children[1]);
 	return contenedor;
 }
+
+// creando textarea
+
 function creandoTextarea(contenedor){
 	var textArea = document.createElement("textarea");
 	contenedor.appendChild(textArea);
 	return textArea;
 }
+
+// creando boton publicar
+
 function agregandoBoton(contenedor){
 	var boton = document.createElement("button");
 	boton.innerText = "publicar";
@@ -105,6 +113,9 @@ function agregandoBoton(contenedor){
 	contenedor.appendChild(boton);
 	return boton;
 }
+
+// creando div para el titulo
+
 function insertarTitulo(valor, content){
 		var titulo = document.createElement("div");
 		titulo.setAttribute("class", "titulo")
@@ -112,6 +123,9 @@ function insertarTitulo(valor, content){
 		content.insertBefore(titulo, content.childNodes[0]);
 		return titulo;
 }
+
+// contenedor de la publicacion
+
 function creandoContenedorPublicados(valor){
 	var contenedorPublicado = document.createElement("div");
 	contenedorPublicado.setAttribute("class", "creandoContenedorPublicados");
@@ -119,24 +133,35 @@ function creandoContenedorPublicados(valor){
 	creandoDivPublicacion(contenedorPublicado, valor);
 	return contenedorPublicado;
 }
+
+// creando contenedor para la imagen
+
 function contentImg(){
 	var contenedorPublicado = document.createElement("div");
 	contenedorPublicado.setAttribute("class", "creandoContenedorPublicados");
 	array[5].insertBefore(contenedorPublicado, array[5].children[2]);
 	return contenedorPublicado;
 }
+// div donde ira el texto
+
 function creandoDivPublicacion(contenedorpublicado, valor){
 	var creandoDiv = document.createElement("div");
 	creandoDiv.innerText = valor;
 	contenedorpublicado.insertBefore(creandoDiv, contenedorpublicado.children[0]);
 	return creandoDiv;
 }
+
+// creando input para los contenedores
+
 function creandoInput(content){
 	var nuevoInput = document.createElement("input");
 	nuevoInput.placeholder = "ESCRIBE EL NOMBRE DEL AUTOR";
 	content.insertBefore(nuevoInput, content.children[0]);
 	return nuevoInput;
 }
+
+// span para el autor
+
 function crearSpan(valor, content){
 	var nuevoSpan = document.createElement("span");
 	nuevoSpan.setAttribute("class", "crearSpan")
@@ -144,6 +169,9 @@ function crearSpan(valor, content){
 	content.insertBefore(nuevoSpan, content.children[1]);
 	return nuevoSpan;
 }
+
+// creando imagen
+
 function creandoImagen(valor, content){
 	var img = document.createElement("img");
 	img.setAttribute("src", valor);
@@ -151,16 +179,25 @@ function creandoImagen(valor, content){
 	content.insertBefore(img, content.children[0]);
 	return img;
 }
+
+// creando el input para la paleta de colores
+
 function cambiarFondo(content){
 	var bobtonColor = document.createElement("input");
 	bobtonColor.setAttribute("type", "color");
 	content.appendChild(bobtonColor);
 	return bobtonColor;
 }
+
+// cambiar el background al contenedor
+
 function colorFondo(content, input){
 	var colorInput = content.style.backgroundColor = input.value;
 	return colorInput;
 }
+
+// funcion para la hora
+
 function hora(contenido){
 	var contHora = document.createElement("div");
 	contHora.setAttribute("class", "contHora")
